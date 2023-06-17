3a6c191ff3325025eb7f30bd1ba2cb6c10228b81
syogo-user <s.photo.trumpet.005@gmail.com>
2023-06-17 19:44:09 +0900

    Initial Commit

---
+++ TCAApp/TCAAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCAAppApp.swift
+//  TCAApp
+//
+//  Created by 小野寺祥吾 on 2023/06/17.
+//
+
+import SwiftUI
+
+@main
+struct TCAAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
